b78d9e31e0411ea933d8f5e4a4b966f627d3140e
iNguyenQuocDoan <quocdoanfb@gmail.com>
2025-10-28 16:04:41 +0700

    Add PUG

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
+// Thiết lập view chứa giao diện
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("client/pages/home", {
+    pageTitle: "Home Page 1",
+  });
+});
+
+app.get("/tour-lists", (req, res) => {
+  res.render("client/pages/tourLists", {
+    pageTitle: "Tour Lists 1",
+  });
 });
 
 app.get("/about", (req, res) => {
