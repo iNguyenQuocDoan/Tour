a7493a54fbbb41fda17c7e2589f492f554d10f44
iNguyenQuocDoan <quocdoanfb@gmail.com>
2025-10-29 20:32:00 +0700

    Connect DB

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const app = express();
 const port = 3000;
+
+mongoose.connect(
+  process.env.DB_CONNECTION_STRING
+);
+
+const Tour = mongoose.model("Tour", {
+  name: String,
+  vehicle: String
+}, "tours");
 
 // Thiết lập view chứa giao diện
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+
+// Thiết lập thư mục chứa file tĩnh
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("client/pages/home", {
@@ -14,9 +29,14 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/tour-lists", (req, res) => {
+app.get("/tour-lists", async (req, res) => {
+  const tourList = await Tour.find({});
+
+  console.log(tourList);
+
   res.render("client/pages/tourLists", {
     pageTitle: "Tour Lists 1",
+    tourList: tourList
   });
 });
 
